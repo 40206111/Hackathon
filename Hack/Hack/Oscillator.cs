7254b07cce3f13b546aa8f359312c8b1165946fa
Paulius Bieksa <priverstinis@gmail.com>
2017-09-06 18:36:14 +0100

    Created oscilator with sine wave patern, note generator class and made an empty class for the sound player.

---
+++ Hack/Hack/Oscillator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hack
+{
+    public static class Oscillator
+    {
+        public static float Sine(float freq, float time)
+        {
+            return (float)Math.Sin(freq * time * 2 * Math.PI);
+        }
+    }
+}
