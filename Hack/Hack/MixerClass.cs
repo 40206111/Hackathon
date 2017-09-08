46f38d9e9d0457243dce292c08c6be4032ef95b0
hamcam98 <hamcalum10@gmail.com>
2017-09-08 13:28:39 +0100

    Added clipping and distortion to MixerClass

--- Hack/Hack/MixerClass.cs
+++ Hack/Hack/MixerClass.cs
@@ -8,8 +8,6 @@ namespace Hack
 {
     public static class MixerClass
     {
-
-
         //Mixing 2 buffers together
         public static List<float> Mix(List<float> FirstBuffer, List<float> SecondBuffer, float FirstWeighting, float SecondWeighting)
         {
@@ -20,8 +18,6 @@ namespace Hack
                 ReturnBuffer.Add((FirstWeighting * FirstBuffer[i]) + (SecondWeighting * SecondBuffer[i]));
             }
             return ReturnBuffer;
-
-
         }
 
         //Normalising the buffer
@@ -31,31 +27,26 @@ namespace Hack
             float largestAbsValue = 0;
             for(int i = 1; i < BufferToNormalize.Count; i++)
             {
-
                 //Finding the largest absolute value
                 if (Math.Abs(BufferToNormalize[i]) > largestAbsValue)
                 {
                     largestAbsValue = Math.Abs(BufferToNormalize[i]);
                 }
             }
-
             //Normalising the buffer
-            for (int i = 0; i < BufferToNormalize.Count; i++ )
+            for (int i = 0; i < BufferToNormalize.Count; i++)
             {
                 NormalizedBuffer.Add(BufferToNormalize[i] / largestAbsValue);
             }
-
-
             return NormalizedBuffer;
         }
 
         //Reverb buffer
         public static List<float> Reverberation(List<float> buffer, float reverbIntensity)
-        {
-            
+        {            
             int length = (int)(44100.0f / 1000.0f * 30.0f);
             List<float> reverbBuffer = new List<float>();
-            for (int i = 0; i < buffer.Count; i++)
+            for (int i = 0; i < buffer.Count; i++) 
             {
                 if (i < length)
                 {
@@ -66,11 +57,27 @@ namespace Hack
                     reverbBuffer.Add(buffer[i - length]);
                 }
             }
-
             reverbBuffer = Mix(buffer, reverbBuffer, 1.0f, reverbIntensity);
-
                 return reverbBuffer;
         }
 
+        //Distortion
+        public static List<float> Distortion(List<float> bufferToClip, float distortionLevel)
+        {
+            //Clipping the buffer
+            for (int i = 1; i < bufferToClip.Count; i++)
+            {
+                bufferToClip[i] = bufferToClip[i] * distortionLevel;
+                if (bufferToClip[i] > 1.0f)
+                {
+                    bufferToClip[i] = 1.0f;
+                }
+                else if (bufferToClip[i] < (-1.0f))
+                {
+                    bufferToClip[i] = -1.0f;
+                }
+            }
+                return bufferToClip;
+        }
     }
 }
